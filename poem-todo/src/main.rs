5cec43f67d0be0a49537bd96ec41528c9d7b374d
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-07-22 19:00:55 +0800

    feat: integrate with poem

--- poem-todo/src/main.rs
+++ poem-todo/src/main.rs
@@ -9,27 +9,48 @@ use crate::schema::*;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
+use poem::web::{Data, Json};
+use poem::{
+    get, handler, listener::TcpListener, web::Path, EndpointExt, IntoResponse, Route,
+    Server,
+};
 use std::env;
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
 
 fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-fn main() {
-    let mut connection = establish_connection();
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[handler]
+fn get_users(mut conn: Data<&Arc<Mutex<PgConnection>>>) -> impl IntoResponse {
     let results = users::table
         .limit(5)
-        .load::<User>(&mut connection)
+        .load::<User>(conn.0.lock().unwrap().deref_mut())
         .expect("Error loading posts");
-    println!("Displaying {} users", results.len());
-    for user in results {
-        println!(
-            "{} {} {} {}",
-            user.id, user.name, user.password, user.is_admin
-        );
+    Json(results)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    dotenv().ok();
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "poem=debug");
     }
+    let connection = establish_connection();
+    let app = Route::new()
+        .at("/hello/:name", get(hello))
+        .at("/users", get(get_users))
+        .data(Arc::new(Mutex::new(connection)));
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .name("poem-todo")
+        .run(app)
+        .await
 }
